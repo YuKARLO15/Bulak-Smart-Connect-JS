3af1f1b422d3d44825005f70d5e001ef090eb4b7
Luan <franciscodgail@gmail.com>
2025-01-05 13:12:22 +0800

    log in page drafts, added some ,ui libraries

---
+++ bulak-smart-connect-js/src/LogInComponents/LogInCard.jsx
@@ -0,0 +1,142 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import Checkbox from "@mui/material/Checkbox";
+import Divider from "@mui/material/Divider";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Link from "@mui/material/Link";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import ForgotPassword from "./ForgotPassword";
+import "./LogInCard.css";
+
+export default function LogInCard() {
+  const [emailError, setEmailError] = React.useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = React.useState("");
+  const [passwordError, setPasswordError] = React.useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleSubmit = (event) => {
+    if (emailError || passwordError) {
+      event.preventDefault();
+      return;
+    }
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  const validateInputs = () => {
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+
+    let isValid = true;
+
+    if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+      setEmailError(true);
+      setEmailErrorMessage("Please enter a valid email address.");
+      isValid = false;
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage("");
+    }
+
+    if (!password.value || password.value.length < 6) {
+      setPasswordError(true);
+      setPasswordErrorMessage("Password must be at least 6 characters long.");
+      isValid = false;
+    } else {
+      setPasswordError(false);
+      setPasswordErrorMessage("");
+    }
+
+    return isValid;
+  };
+
+  return (
+    <Card className="LogInCardContainer">
+      <Typography variant="h4" className="LogInTitle">
+        LOG IN
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        noValidate
+        className="LogInForm"
+      >
+        <FormControl>
+          <FormLabel htmlFor="email" className="LogInLabel">
+            Email
+          </FormLabel>
+          <TextField
+            error={emailError}
+            helperText={emailErrorMessage}
+            id="email"
+            type="email"
+            name="email"
+            placeholder="your@email.com"
+            autoComplete="email"
+            autoFocus
+            required
+            fullWidth
+            variant="outlined"
+            className={`TextField ${emailError ? "error" : ""}`}
+          />
+        </FormControl>
+        <FormControl>
+          <Box className="PasswordContainer">
+            <FormLabel htmlFor="password" className="LogInLabel">
+              Password
+            </FormLabel>
+            <Link
+              component="button"
+              type="button"
+              onClick={handleClickOpen}
+              variant="body2"
+              className="ForgotPassword"
+            >
+              Forgot your password?
+            </Link>
+          </Box>
+          <TextField
+            error={passwordError}
+            helperText={passwordErrorMessage}
+            name="password"
+            placeholder="••••••"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            required
+            fullWidth
+            className={`TextField ${passwordError ? "error" : ""}`}
+          />
+        </FormControl>
+        <FormControlLabel
+          control={<Checkbox value="remember" color="#184a5b" />}
+          label="Remember me"
+          className="RememberMe"
+        />
+        <ForgotPassword open={open} handleClose={handleClose} />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          onClick={validateInputs}
+          className="LoginButton"
+        >
+          Log In
+        </Button>
+      </Box>
+    </Card>
+  );
+}
+
