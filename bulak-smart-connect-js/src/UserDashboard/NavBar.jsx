1a0e420ed44571035583236549e4b19463b48433
Luan <franciscodgail@gmail.com>
2025-01-12 14:44:47 +0800

    Edited Nav Bar

--- bulak-smart-connect-js/src/UserDashboard/NavBar.jsx
+++ bulak-smart-connect-js/src/UserDashboard/NavBar.jsx
@@ -5,14 +5,21 @@ const NavBar = () => {
   return (
     <nav className="sidebar">
       <div className="NavigationBar">
-        <a href="/">username</a>
+        <div className="Profile">
+          <typography className="Username" >username</typography>
+          <typography className="UserEmail" >User@gmail.com</typography>
+        </div>
+        <div className="NavigationButtons">
         <a href="/dashboard">Dashboard</a>
         <a href="/appointments">Appointments</a>
         <a href="/document_application">Document Application</a>
         <a href="/qrcode">QR Code</a>
         <a href="/account">Account</a>
         <a href="/settings">Settings</a>
-        <a href="/logout">Log Out</a>
+        </div>
+        <div className="Logout">
+          <a href="/logout">Log Out</a>
+          </div>
       </div>
 
     </nav>
