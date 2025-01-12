9286b3e1ccf15557932bc56a574119f70bdf826c
jhazminereigne <127829766+jhazminereigne@users.noreply.github.com>
2025-01-12 21:01:01 +0800

    sana maayos na

--- bulak-smart-connect-js/src/App.jsx
+++ bulak-smart-connect-js/src/App.jsx
@@ -1,25 +1,21 @@
 import { Route, Routes } from 'react-router-dom'; // Import necessary components from react-router-dom
 import './App.css';
-import SignupForm from './pages/SignUpForm'; 
+import SignUpForm from './pages/SignUpForm'; 
 import LogIn from './LogInComponents/LogIn';  
 import UserDashboard from './UserDashboard/UserDashboard'; 
 
 
 
-function App() {
- 
-  return (
- <div><SignUpForm></SignUpForm></div>
-  )
+
 function App() {
   return (
     <Routes>
       {/* Define Routes */}
       <Route path="/" element={<LogIn />} />
-      <Route path='/SignUpForm' element={<SignupForm />} />
+      <Route path='/SignUpForm' element={<SignUpForm />} />
       <Route path='/UserDashBoard' element={<UserDashboard />} />
     </Routes>
   );
 }
 
-export default App;
+export default App
