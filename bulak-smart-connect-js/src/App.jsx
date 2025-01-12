484da1c25fc7d3340abb5d7bcde96a614f7a0b29
jhazminereigne <127829766+jhazminereigne@users.noreply.github.com>
2025-01-12 20:12:03 +0800

    dropdown

--- bulak-smart-connect-js/src/App.jsx
+++ bulak-smart-connect-js/src/App.jsx
@@ -4,13 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import SignUpForm from './pages/SignUpForm.jsx'
 
-function App() {
-return <div><SignUpForm>SignUpForm</SignUpForm></div>
-import LogIn from './LogInComponents/LogIn'
+
 function App() {
  
   return (
- <div><LogIn></LogIn></div>
+ <div><SignUpForm></SignUpForm></div>
   )
 }
 
