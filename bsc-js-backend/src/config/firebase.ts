9fdc58e4731ca96e296c0a02c9bb3a7cebb11b08
YuKARLO15 <147626443+YuKARLO15@users.noreply.github.com>
2025-02-02 16:31:00 +0800

    fixed: firebase.ts

--- bsc-js-backend/src/config/firebase.ts
+++ bsc-js-backend/src/config/firebase.ts
@@ -1,5 +1,10 @@
 import * as admin from 'firebase-admin';
-import * as serviceAccount from './serviceAccountKey.json';
+import * as fs from 'fs';
+
+// Load the service account key
+const serviceAccount = JSON.parse(
+  fs.readFileSync('serviceAccountKey.json', 'utf-8')
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
@@ -8,6 +13,9 @@ admin.initializeApp({
 
 // Connect Firestore to emulator
 const firestore = admin.firestore();
+export const auth = admin.auth();
+export const storage = admin.storage().bucket();
+
 firestore.settings({
   host: 'localhost:8081', // Default emulator port
   ssl: false,
