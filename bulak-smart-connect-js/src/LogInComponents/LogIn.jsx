fc207e0caf227dd400dea97144e0c15cf7e9c1d6
YuKARLO15 <147626443+YuKARLO15@users.noreply.github.com>
2025-02-28 19:02:56 +0800

    Implemented that when logged in, it doesn't go to the login page

--- bulak-smart-connect-js/src/LogInComponents/LogIn.jsx
+++ bulak-smart-connect-js/src/LogInComponents/LogIn.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 import "./LogIn.css";
 import LogInContent from "./LogInContent";
 import LogInCard from "./LogInCard";
@@ -6,7 +7,7 @@ import NavBar from "./NavBar";
 import { useAuth } from "../AuthContext"; // Import useAuth
 
 function LogIn() {
-  const { login } = useAuth(); // Use the login function from AuthContext
+  const { isAuthenticated, login } = useAuth(); // Use the login function from AuthContext
 
   const handleLogin = (data) => {
     // Handle the login data, e.g., save the user session
@@ -14,16 +15,22 @@ function LogIn() {
     login(); // Set the authentication state to true
   };
 
+  // Redirect to UserDashboard if already authenticated
+  if (isAuthenticated) {
+    return <Navigate to="/UserDashboard" />;
+  }
+
   return (
     <div className="LogIn">
       <div className="Navigation">
         <NavBar />
       </div>
       <div className="LogInContainer">
-        <LogInContent></LogInContent>
-        <LogInCard onLogin={handleLogin}></LogInCard>
+        <LogInContent />
+        <LogInCard onLogin={handleLogin} />
       </div>
     </div>
   );
 }
+
 export default LogIn;
