021cab9502397c3435b856ca9cdb8fc623cc0a5d
YuKARLO15 <147626443+YuKARLO15@users.noreply.github.com>
2025-05-06 23:28:13 +0800

    refactor: simplify mock navigation implementation in Storybook

--- bulak-smart-connect-js/.storybook/preview.jsx
+++ bulak-smart-connect-js/.storybook/preview.jsx
@@ -2,27 +2,26 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { AuthProvider } from '../src/context/AuthContext';
 
-// Create a mock of react-router-dom's useNavigate
-import * as router from 'react-router-dom';
+// Create a mock navigate function 
+const mockNavigate = (...args) => {
+  console.log('Navigation called in Storybook with:', args);
+  return false;
+};
+
+// Override React Router's useNavigate
+// This needs to be done before any components try to use it
+import * as ReactRouterDOM from 'react-router-dom';
+ReactRouterDOM.useNavigate = () => mockNavigate;
 
 // Global decorator to wrap all stories in a Router
 export const decorators = [
-  (Story) => {
-    // Create a mock navigate function that will be available in stories
-    const navigate = jest.fn();
-
-    // Override the useNavigate hook before rendering
-    // This needs to be done before rendering the component
-    jest.spyOn(router, 'useNavigate').mockImplementation(() => navigate);
-
-    return (
-      <MemoryRouter>
-        <AuthProvider>
-          <Story />
-        </AuthProvider>
-      </MemoryRouter>
-    );
-  },
+  (Story) => (
+    <MemoryRouter initialEntries={['/']}>
+      <AuthProvider>
+        <Story />
+      </AuthProvider>
+    </MemoryRouter>
+  ),
 ];
 
 export const parameters = {
