3ee1c5933be9863de67f80f28b92419561eb4f79
YuKARLO15 <147626443+YuKARLO15@users.noreply.github.com>
2025-05-25 23:15:11 +0800

    fix: update API_BASE_URL assignment to use commented placeholder for environment variable

--- bulak-smart-connect-js/src/services/announcementService.js
+++ bulak-smart-connect-js/src/services/announcementService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+const API_BASE_URL = /*import.meta.env.VITE_REACT_APP_API_URL || Temporary Comment will setup later*/ 'http://localhost:3000';
 
 export const announcementService = {
   // Get all announcements
