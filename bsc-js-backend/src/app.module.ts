ce7aa888de9530ec4bd6b4482750933bb509f010
YuKARLO15 <147626443+YuKARLO15@users.noreply.github.com>
2025-05-25 21:14:43 +0800

    add announcements module with controller, service, and DTOs

--- bsc-js-backend/src/app.module.ts
+++ bsc-js-backend/src/app.module.ts
@@ -9,6 +9,7 @@ import { AppService } from './app.service';
 import { RolesModule } from './roles/roles.module';
 import { QueueModule } from './modules/queue/queue.module';
 import { AppointmentModule } from './modules/appointment/appointment.module';
+import { AnnouncementsModule } from './modules/announcements/announcements.module';
 
 @Module({
   imports: [
@@ -60,6 +61,7 @@ import { AppointmentModule } from './modules/appointment/appointment.module';
     RolesModule,
     QueueModule,
     AppointmentModule,
+    AnnouncementsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
