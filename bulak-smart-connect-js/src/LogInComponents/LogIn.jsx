aaa16986a6f546d27d08df2d2f68e846a1febb51
YuKARLO15 <147626443+YuKARLO15@users.noreply.github.com>
2025-06-05 16:25:50 +0800

    Fix import casing for HeroBg in LogIn component

--- bulak-smart-connect-js/src/LogInComponents/LogIn.jsx
+++ bulak-smart-connect-js/src/LogInComponents/LogIn.jsx
@@ -5,7 +5,7 @@ import LogInContent from './LogInContent';
 import LogInCard from './LogInCard';
 import NavBar from '../NavigationComponents/NavBar';
 import { useAuth } from '../context/AuthContext'; // Import useAuth
-import HeroBG from '../LandingPageComponents/LandingPageAssets/HeroBG.JPEG';
+import HeroBg from '../LandingPageComponents/LandingPageAssets/HeroBg.JPEG'; 
 
 function LogIn() {
   const { isAuthenticated, user } = useAuth(); // Use the login function from AuthContext
